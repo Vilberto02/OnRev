411f59fafeaad882b34ff9953c3711303483cc45
GiacomoMadrid <gsmr.18@gmail.com>
2025-06-10 19:10:41 -0500

    clases

---
+++ Source/OnRev/src/modelo/EstructuraControl.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Giacomo
+ */
+public class EstructuraControl {
+    private String sintaxis;
+    private String nombre;
+    private int nivelAnidamiento;
+    
+    public EstructuraControl(String str, int n){
+        this.nombre = str;
+        this.nivelAnidamiento = n;
+        
+    
+    }
+    
+    // ******************************* Métodos *********************************
+    public int getNivelAnidamiento(){
+        return this.getNivelAnidamiento();
+    }
+}
