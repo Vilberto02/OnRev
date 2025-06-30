9c9b445dd8dc914c0e55a4c214b86d1b952d4d07
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-06-30 17:37:38 -0500

    Agregar algoritmo predefinido

--- Source/OnRev/src/modelo/AlgoritmoPredefinido.java
+++ Source/OnRev/src/modelo/AlgoritmoPredefinido.java
@@ -4,7 +4,9 @@
  */
 package modelo;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  *
@@ -22,10 +24,75 @@ public class AlgoritmoPredefinido extends Pseudocodigo{
     // ******************************* Métodos *********************************
     
     public Long ejecutarConEntrada(int n){
-        //TO DO
-        return null;
+        int[] arreglo = generarArregloAleatorio(n);
+
+        long inicio = System.nanoTime();
+
+        // El algoritmo se ejecutará segun el nombre
+        if (this.nombre.equalsIgnoreCase("BubbleSort")) {
+            bubbleSort(arreglo);
+        } else if (this.nombre.equalsIgnoreCase("QuickSort")) {
+            quickSort(arreglo, 0, arreglo.length - 1);
+        }
+
+        long fin = System.nanoTime();
+        long duracion = fin - inicio;
+
+        if (tiemposReales == null) {
+            tiemposReales = new HashMap<>();
+        }
+
+        tiemposReales.put(n, duracion);
+        return duracion;
     }
     
+    private int[] generarArregloAleatorio(int n) {
+        Random rand = new Random();
+        int[] arreglo = new int[n];
+        for (int i = 0; i < n; i++) {
+            arreglo[i] = rand.nextInt(1000);
+        }
+        return arreglo;
+    }
+    
+    private void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int aux = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = aux;
+                }
+            }
+        }
+    }
+    
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = particion(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    
+    private int particion(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
     public ComparacionResultado comparar(FuncionTiempo f){
         //TO DO
         return null;
